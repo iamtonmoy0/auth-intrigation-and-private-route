7e109dd3c58f9cd355a6d6d1b9bd0a6dc1414055
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-19 18:44:14 -0400

    feat: header and footer components added and routes set

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Main from './layout/Main'
+import Home from './components/Home/Home'
+
+
+
+const routes=createBrowserRouter([
+	{
+	path:'/',
+	element:<Main/>,
+	children:[
+		{
+		path:'/',
+		element:<Home/>
+		},
+		]
+	}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes}>
+     
+    </RouterProvider>
   </React.StrictMode>,
 )
