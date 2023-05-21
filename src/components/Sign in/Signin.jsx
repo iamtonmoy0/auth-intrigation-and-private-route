cbd4d205663cdc50b5b33fedda5592456fb0f3b7
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-21 00:14:04 -0400

    feat: sign in and login auth added

--- src/components/Sign in/Signin.jsx
+++ src/components/Sign in/Signin.jsx
@@ -1,6 +1,28 @@
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Signin = () => {
+const {signIn}=useContext(AuthContext)
+const [success,setSuccess]=useState('')
+const handleSignIn=(e)=>{
+  e.preventDefault();
+  const form=e.target;
+  const mail=form.email.value;
+  const pass=form.password.value;
+  signIn(mail,pass)
+  .then(result=>{
+    console.log(result.user)
+    setSuccess('Login successfully')
+  })
+  .catch(error=>{
+    console.log(error)
+    
+  })
+
+}
+
+
 	return (
 		<div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,18 +31,18 @@ const Signin = () => {
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+      <form className="card-body" onSubmit={handleSignIn}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="text" placeholder="email" className="input input-bordered" name="email" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" />
+          <input type="password" placeholder="password" className="input input-bordered" name="password" />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
@@ -29,7 +51,8 @@ const Signin = () => {
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
         </div>
-      </div>
+        <p>{success}</p>
+      </form>
     </div>
   </div>
 </div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import {getAuth,  createUserWithEmailAndPassword } from 'firebase/auth';
+import {getAuth,  createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import app from "../Firebase/Firebase.config";
 
 const auth =getAuth(app)
@@ -8,13 +8,17 @@ export const AuthContext=createContext();
 const AuthProvider =({children}) => {
 	const [user,setUser]=useState(null)
        const createUser=(mail,pass)=>{
+        return createUserWithEmailAndPassword(auth,mail,pass)
+       }
 
-	return createUserWithEmailAndPassword(auth,mail,pass)
+       const signIn=(mail,pass)=>{
+	return signInWithEmailAndPassword(auth,mail,pass)
        }
 
 	const authInfo={
 		user,
 		createUser,
+		signIn,
 	}
 	return (
 		<>
